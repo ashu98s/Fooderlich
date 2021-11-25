c9d4b0fdb07e82a0b824b3c878dee1806e087548
ashu98s <ashirbad.dkl24@gmail.com>
2021-11-25 19:51:08 +0530

    adding card3

---
+++ lib/card3.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+
+class Card3 extends StatelessWidget {
+  const Card3({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag2.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        child: Stack(
+          children: [
+            // TODO 5: add dark overlay BoxDecoration
+            Container(
+              decoration: BoxDecoration(
+                // 1
+                color: Colors.black.withOpacity(0.6),
+                // 2
+                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+              ),
+            ),
+            // TODO 6: Add Container, Column, Icon and Text
+            Container(
+              // 3
+              padding: const EdgeInsets.all(16),
+              // 4
+              child: Column(
+                // 5
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // 6
+                  const Icon(
+                    Icons.book,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  // 7
+                  const SizedBox(height: 8),
+                  // 8
+                  Text(
+                    'Recipe Trends',
+                    style: FooderlichTheme.darkTextTheme.headline2,
+                  ),
+                  // 9
+                  const SizedBox(height: 30),
+                ],
+              ),
+            ),
+            // TODO 7: Add Center widget with Chip widget children
+            Center(
+              // 11
+              child: Wrap(
+                // 12
+                alignment: WrapAlignment.start,
+                // 13
+                spacing: 12,
+                // 14
+                children: [
+                  Chip(
+                    label: Text('Healthy',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: () {
+                      print('delete');
+                    },
+                  ),
+                  Chip(
+                    label: Text('Vegan',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: () {
+                      print('delete');
+                    },
+                  ),
+                  Chip(
+                    label: Text('Carrots',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                  ),
+                  Chip(
+                    label: Text('Greens',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                  ),
+                  Chip(
+                    label: Text('Wheat',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                  ),
+                  Chip(
+                    label: Text('Pescetarian',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                  ),
+                  Chip(
+                    label: Text('Mint',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                  ),
+                  Chip(
+                    label: Text('Lemongrass',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                  ),
+                  Chip(
+                    label: Text('Pastry',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                  ),
+                  Chip(
+                    label: Text('Cake',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                  ),
+                  Chip(
+                    label: Text('Juice',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                  ),
+                  Chip(
+                    label: Text('Hot Milk',
+                        style: FooderlichTheme.darkTextTheme.bodyText1),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

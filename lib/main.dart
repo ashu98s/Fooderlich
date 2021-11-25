d9e8cc110da26a5feb82ab67a26291c9b76dd13f
ashu98s <ashirbad.dkl24@gmail.com>
2021-11-25 13:41:36 +0530

    Creating Home Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
 
 void main() {
-  // 1
   runApp(const Fooderlich());
 }
 
 class Fooderlich extends StatelessWidget {
-  // 2
   const Fooderlich({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     // TODO: Create theme
+    final theme = FooderlichTheme.dark();
     // TODO: Apply Home widget
-    // 3
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // TODO: Add theme
+      theme: theme,
       title: 'Fooderlich',
-      // 4
+
       home: Scaffold(
         // TODO: Style the title
-        appBar: AppBar(title: const Text('Fooderlich')),
+        appBar: AppBar(
+          title: Text('Fooderlich', style: theme.textTheme.headline6),
+        ),
         // TODO: Style the body text
-        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
+        body: Center(
+          child: Text('Let\'s get cooking 👩‍🍳',
+              style: theme.textTheme.headline1),
+        ),
       ),
     );
   }

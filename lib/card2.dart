acabf856f77ebd6ec57acd241f1aa7f04f8a1fe2
ashu98s <ashirbad.dkl24@gmail.com>
2021-11-25 19:50:40 +0530

    adding card2

---
+++ lib/card2.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'author_card.dart';
+import 'fooderlich_theme.dart';
+
+class Card2 extends StatelessWidget {
+  const Card2({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      // 1
+      child: Container(
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag5.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+        ),
+        // 2
+        child: Column(
+          children: [
+            // TODO 1: add author information
+            const AuthorCard(
+              authorName: 'Mike Katz',
+              title: 'Smoothie Connoisseur',
+              imageProvider: AssetImage('assets/author_katz.jpeg'),
+            ),
+            // TODO 4: add Positioned text
+            // 1
+            Expanded(
+              // 2
+              child: Stack(
+                children: [
+                  // 3
+                  Positioned(
+                    bottom: 16,
+                    right: 16,
+                    child: Text(
+                      'Recipe',
+                      style: FooderlichTheme.lightTextTheme.headline1,
+                    ),
+                  ),
+                  // 4
+                  Positioned(
+                    bottom: 70,
+                    left: 16,
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Text(
+                        'Smoothies',
+                        style: FooderlichTheme.lightTextTheme.headline1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

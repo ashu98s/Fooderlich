0e8b419567795a1467dac496f4a1b66aad168d1b
ashu98s <ashirbad.dkl24@gmail.com>
2021-11-25 19:50:00 +0530

    making changes

---
+++ lib/author_card.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'circle_image.dart';
+
+class AuthorCard extends StatelessWidget {
+  // 1
+  final String authorName;
+  final String title;
+  final ImageProvider? imageProvider;
+
+  const AuthorCard({
+    Key? key,
+    required this.authorName,
+    required this.title,
+    this.imageProvider,
+  }) : super(key: key);
+
+  // 2
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Replace return Container(...);
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        // TODO 3: add alignment
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // 1
+          Row(
+            children: [
+              CircleImage(
+                imageProvider: imageProvider,
+                imageRadius: 28,
+              ),
+              // 2
+              const SizedBox(width: 8),
+              // 3
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    authorName,
+                    style: FooderlichTheme.lightTextTheme.headline2,
+                  ),
+                  Text(
+                    title,
+                    style: FooderlichTheme.lightTextTheme.headline3,
+                  )
+                ],
+              ),
+            ],
+          ),
+          // TODO 2: add IconButton
+          IconButton(
+            // 4
+              icon: const Icon(Icons.favorite_border),
+              iconSize: 30,
+              color: Colors.grey[400],
+              // 5
+              onPressed: () {
+                const snackBar = SnackBar(content: Text('Favorite Pressed'));
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              }),
+        ],
+      ),
+    );
+  }
+}

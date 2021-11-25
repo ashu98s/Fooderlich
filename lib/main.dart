5e70a47eb18417a484202cd8d36dcb6fc9b64db8
ashu98s <ashirbad.dkl24@gmail.com>
2021-11-25 13:16:50 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // 1
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  // 2
+  const Fooderlich({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Create theme
+    // TODO: Apply Home widget
+    // 3
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // TODO: Add theme
+      title: 'Fooderlich',
+      // 4
+      home: Scaffold(
+        // TODO: Style the title
+        appBar: AppBar(title: const Text('Fooderlich')),
+        // TODO: Style the body text
+        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
+      ),
+    );
+  }
+}
